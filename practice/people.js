e8d9920251f39c37bf5546e7adc60d675cf49a6f
Marius CA <marius@example.com>
2023-02-22 15:01:25 +0200

    man print

---
+++ practice/people.js
@@ -0,0 +1,70 @@
+'use strict';
+console.log('people.js file was loaded');
+
+const people = [
+  {
+    name: 'Jonas',
+    surname: 'Jonaitis',
+    sex: 'male',
+    age: 26,
+    income: 1200,
+    married: false,
+    hasCar: false,
+  },
+  {
+    name: 'Severija',
+    surname: 'Piktutytė',
+    sex: 'female',
+    age: 26,
+    income: 1300,
+    married: false,
+    hasCar: true,
+  },
+  {
+    name: 'Valdas',
+    surname: 'Vilktorinas',
+    sex: 'male',
+    age: 16,
+    income: 0,
+    married: false,
+    hasCar: false,
+  },
+  {
+    name: 'Virginijus',
+    surname: 'Uostauskas',
+    sex: 'male',
+    age: 32,
+    income: 2400,
+    married: true,
+    hasCar: true,
+  },
+  {
+    name: 'Samanta',
+    surname: 'Uostauskienė',
+    sex: 'female',
+    age: 28,
+    income: 1200,
+    married: true,
+    hasCar: true,
+  },
+  {
+    name: 'Janina',
+    surname: 'Stalautinskienė',
+    sex: 'female',
+    age: 72,
+    income: 364,
+    married: false,
+    hasCar: false,
+  },
+];
+
+// 1. Naudojant Array.prototype.forEach atspausdinti visus vyrus.
+
+people.forEach((pObj) => {
+  // ar vyras
+  if (pObj.sex === 'male') {
+    console.log('pObj man ===', pObj);
+  }
+});
+
+// 2. Naudojant Array.prototype.filter grazinti visas moteris jaunesnes nei 35 metai.
